0a19eaba543238d142e0c6564f524da0a6beb46f
Carl Arne Thomann <arnethomann@me.com>
2015-12-08 15:05:23 +0100

    Mikrowellen: Text der Auswertung ist fertig.
    Referenzen können gesetzt werden, wenn Theorie und Durcführung kommen

--- Mikrowellen/pc/plot.py
+++ Mikrowellen/pc/plot.py
@@ -12,6 +12,7 @@ from uncertainties.unumpy import nominal_values as noms
 from uncertainties.unumpy import std_devs as sdevs
 import scipy.constants as con
 from scipy.constants import physical_constants as pcon
+
 #Table Funktion
 dummy = ufloat(69,42)
 dummyarray = np.array([dummy,dummy*6.626])
@@ -75,10 +76,12 @@ def table(name, data):
 def quad(x,a,b,c):
 	return a*x**2+b*x+c
 
+#Modenparameter
 mod1=np.array([200,220,230,0,2*3.15,0])
 mod2=np.array([120,140,150,0,2*3.15,0])
 mod3=np.array([70,80,90,0,2*2.65,0])
 
+#Quadratische Regression der Moden
 params1,comat = curve_fit(quad,mod1[0:3],mod1[3:6])
 params2,comat = curve_fit(quad,mod2[0:3],mod2[3:6])
 params3,comat = curve_fit(quad,mod3[0:3],mod3[3:6])
@@ -95,7 +98,7 @@ print(max(quad(x,*params3)))
 with open("pc/resultat.txt", 'w') as data:
 	data.write("#Parameter der Fits\n" + "%s\n%s\n%s\n"%(params1,params2,params3) + "#Maxima der Fits\n" +"%s\n%s\n%s\n"%(max(quad(x,*params1)),max(quad(x,*params2)),max(quad(x,*params3))))
 
-
+# Modenplot
 plt.xlim(50,250)
 plt.ylim(0,8)
 plt.plot(mod1[0:3],mod1[3:6],'kx', label='Messdaten')
@@ -109,10 +112,43 @@ plt.ylabel(r'Ma\ss\,\,f\"ur Leistung, Amplitude $A$')
 plt.legend(loc=0,numpoints=1)
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('pc/plot.pdf')
-plt.show()
+#plt.show()
 
+#Mittelwerte der Breitseite
 breiteseite_mean=np.mean([22.45,22.7,22.7,22.7,22.55])
 print(breiteseite_mean)
 
 with open("pc/resultat.txt", 'a') as data:
 	data.write("#Breitseite in mm\n"+"%s\n"%breiteseite_mean)
+
+#Plot und Fit der Theorie-Dämpfung
+dat=np.array([1.345,1.698,2.115,2.341,2.609])
+mm=np.array([0,1,2,3,4,5])
+dB=np.array([0,2,8,17,29,46])
+plt.plot(mm,dB,"bx",label="Theorie-Dämpfung")
+params,coeff=curve_fit(quad,mm,dB)
+plt.plot(dat,np.array([2,4,6,8,10]),"rx",label="Messwerte")
+plt.plot(np.linspace(0,5),quad(np.linspace(0,5),*params),"b-",label="Fit der Theorie-Dämpfung")
+plt.legend(loc="best",numpoints=1)
+plt.grid()
+plt.xlabel(r'D\"ampfungseinstellung $\:/\: \si{\milli\meter}$')
+plt.ylabel(r'D\"ampfung $\:/\: \si{\decibel}$')
+plt.savefig('pc/daempfung.pdf')
+plt.show()
+
+with open("pc/resultat.txt", 'a') as data:
+	data.write("#Fit der Theorie-Dämpfung\n"+"%s\n"%params)
+
+#Berechnung der Frequenz
+lambda_g=48.2
+a=22.62
+freq=con.value("speed of light in vacuum")*np.sqrt(1/lambda_g**2 + 1/(4*a**2))
+print(freq)
+with open("pc/resultat.txt", 'a') as data:
+	data.write("#Berechnete Frequenz\n"+"%s\n"%freq)
+
+dat=np.array([1.345,1.698,2.115,2.341,2.609])
+print(quad(dat,*params))
+print(np.array([2,4,6,8,10])-quad(dat,*params)/quad(dat,*params)*100)
+with open("pc/resultat.txt", 'a') as data:
+	data.write("#Theoriewerte\n"+"%s\n"%quad(dat,*params))
