231657aebfe5fa5ceca5bb3c6b15ee9316d93aa7
Carl Arne Thomann <arnethomann@me.com>
2016-01-30 17:34:51 +0100

    Benenne Daten von Robert um. Leerwürfel_gerade wird einzeln betrachtet.

--- Tomographie/pc/plot.py
+++ Tomographie/pc/plot.py
@@ -12,6 +12,8 @@ from uncertainties.unumpy import nominal_values as noms
 from uncertainties.unumpy import std_devs as sdevs
 import scipy.constants as con
 from scipy.constants import physical_constants as pcon
+import os, os.path
+
 #Table Funktion
 dummy = ufloat(69,42)
 dummyarray = np.array([dummy,dummy*6.626])
@@ -21,72 +23,68 @@ udummyarray = unp.uarray([4], [5*con.pi])
 #p1 = {'name':'tabelle1.tex','data':data1}
 #table(**p1)
 def table(name, data):
-	j=0
-	i=0
-	f = [0,0,0,0,0,0,0,0,0,0,0,0,0]
-	for i in range(len(data)):
-		if(type(data[i][0]) == type(dummy) or type(data[i][0]) == type(dummyarray[1]) or type(data) == type(udummyarray)):
-			f[i] = True
-		else:
-			f[i] = False
-	print(f)
-	output = open(name, 'w')
-	output.write(r'\begin{table}[h]' + '\n' + r'\centering' + '\n' + r'\caption{CAPTION}' + '\n' +r'\sisetup{%uncertainty-seperator = {\,},'+'\n'+r'table-number-alignment = center,'+'\n'+'table-unit-alignment = center,'+'\n'+'%table-figures-integer = 1,'+'\n'+'%table-figures-decimal = 1,'+'\n'+'table-auto-round = true'+'\n'+'}'+'\n'+ r'\begin{tabular}{ ')
-	for i in range(len(data)):
-		if(f[i]):
-			output.write(r'S[table-format= 3.1]'+'\n'+' @{\,$\pm{}$\,} '+'\n' + r' S[table-format= 3.1] ')
-		else:
-			output.write(r' S[table-format= 3.1] '+'\n')
-	output.write(r'}' + '\n' + r'\toprule' + '\n')
-	
-	for i in range(len(data)):
-		if(i < (len(data)-1)): 
-			if(f[i]):
-				output.write(r'\multicolumn{2}{c}{TITLE}'+'\n'+'&')
-			else:
-				output.write(r'{$\text{Title}$}'+'\n'+'&')
-		else:
-			if(f[i]):
-				output.write(r'\multicolumn{2}{c}{TITLE} \\'+'\n')
-			else:
-				output.write(r'{$\text{Title}$} \\'+'\n')
-	output.write(r' \midrule' + '\n')
-	
-	#Tabelle
-	for j in range(len(data[0])):
-		i = 0
-		while i <= len(data)-1:
-				if(f[i]):
-					if(i == len(data)-1):
-						output.write(str(data[i][j].n) + '&' + str(data[i][j].s) + r'\\' + '\n')
-					else:
-						output.write(str(data[i][j].n)+ '&' + str(data[i][j].s) + '&')
-					i = i+1
-				else:
-					if(i == len(data)-1):
-						output.write(str(data[i][j]) + r'\\' + '\n')
-					else:
-						output.write(str(data[i][j]) + '&')
-					i = i+1
-	#Tabelle Ende
-	output.write(r'\bottomrule' + '\n' + r'\end{tabular}' + '\n' + r'\label{tab:LABEL}' + '\n' + r'\end{table}')
-	output.close()
-	
+    j=0
+    i=0
+    f = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+    for i in range(len(data)):
+        if(type(data[i][0]) == type(dummy) or type(data[i][0]) == type(dummyarray[1]) or type(data) == type(udummyarray)):
+            f[i] = True
+        else:
+            f[i] = False
+    print(f)
+    output = open(name, 'w')
+    output.write(r'\begin{table}[h]' + '\n' + r'\centering' + '\n' + r'\caption{CAPTION}' + '\n' +r'\sisetup{%uncertainty-seperator = {\,},'+'\n'+r'table-number-alignment = center,'+'\n'+'table-unit-alignment = center,'+'\n'+'%table-figures-integer = 1,'+'\n'+'%table-figures-decimal = 1,'+'\n'+'table-auto-round = true'+'\n'+'}'+'\n'+ r'\begin{tabular}{ ')
+    for i in range(len(data)):
+        if(f[i]):
+            output.write(r'S[table-format= 3.1]'+'\n'+' @{\,$\pm{}$\,} '+'\n' + r' S[table-format= 3.1] ')
+        else:
+            output.write(r' S[table-format= 3.1] '+'\n')
+    output.write(r'}' + '\n' + r'\toprule' + '\n')
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+    for i in range(len(data)):
+        if(i < (len(data)-1)):
+            if(f[i]):
+                output.write(r'\multicolumn{2}{c}{TITLE}'+'\n'+'&')
+            else:
+                output.write(r'{$\text{Title}$}'+'\n'+'&')
+        else:
+            if(f[i]):
+                output.write(r'\multicolumn{2}{c}{TITLE} \\'+'\n')
+            else:
+                output.write(r'{$\text{Title}$} \\'+'\n')
+    output.write(r' \midrule' + '\n')
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$Y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+    #Tabelle
+    for j in range(len(data[0])):
+        i = 0
+        while i <= len(data)-1:
+                if(f[i]):
+                    if(i == len(data)-1):
+                        output.write(str(data[i][j].n) + '&' + str(data[i][j].s) + r'\\' + '\n')
+                    else:
+                        output.write(str(data[i][j].n)+ '&' + str(data[i][j].s) + '&')
+                    i = i+1
+                else:
+                    if(i == len(data)-1):
+                        output.write(str(data[i][j]) + r'\\' + '\n')
+                    else:
+                        output.write(str(data[i][j]) + '&')
+                    i = i+1
+    #Tabelle Ende
+    output.write(r'\bottomrule' + '\n' + r'\end{tabular}' + '\n' + r'\label{tab:LABEL}' + '\n' + r'\end{table}')
+    output.close()
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
 
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('pc/plot.pdf')
+
+for root, _, files in os.walk("rawdata/Daten_von_Robert/"):
+    for f in files:
+        fullpath = os.path.join(root, f)
+        print(fullpath)
+        x,y = np.genfromtxt("{}".format(fullpath), unpack=True)
+        plt.plot(x,y,"k-",label="{}".format(f))
+        plt.legend(loc='best')
+        plt.xlim(0,len(x))
+        plt.ylim(0,max(y)+100)
+        plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+        plt.savefig('pc/{}.pdf'.format(f))
+        plt.close()
